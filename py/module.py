f1f12adffd4eec1173b9a928136600107832eb32
Enigmatisms <984041003@qq.com>
2022-03-23 01:14:42 +0800

    Network architecture completed

--- py/module.py
+++ py/module.py
@@ -4,6 +4,7 @@
     NeRF network details. To be finished ...
 """
 
+from re import M
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -14,10 +15,11 @@ def makeMLP(in_chan, out_chan, act = nn.ReLU, batch_norm = False):
     modules = [nn.Linear(in_chan, out_chan)]
     if batch_norm == True:
         modules.append(nn.BatchNorm1d(out_chan))
-    if act == True:
-        modules.append(nn.ReLU(inplace = True))
+    if not act is None:
+        modules.append(act)
     return modules
 
+# This module is shared by coarse and fine network, with no need to modify
 class NeRF(nn.Module):
     def __init__(self, position_flevel, direction_flevel) -> None:
         super().__init__()
@@ -25,16 +27,27 @@ class NeRF(nn.Module):
         self.direction_flevel = direction_flevel
 
         module_list = makeMLP(60, 256)
-        # for i in range(7):
+        for _ in range(4):
+            module_list.extend(makeMLP(256, 256))
 
-        self.lin_body = nn.Sequential(
-            
+        self.lin_block1 = nn.Sequential(*module_list)       # MLP before skip connection
+        self.lin_block2 = nn.Sequential(
+            *makeMLP(316, 256),
+            *makeMLP(256, 256), *makeMLP(256, 256),
+            *makeMLP(256, 256, None)
         )
 
-    # Positional Encoding is implemented through CUDA
+        self.opacity_head = nn.Sequential(                  # authors said that ReLU is used here
+            *makeMLP(256, 1)
+        )
+        self.rgb_layer = nn.Sequential(
+            *makeMLP(280, 128),
+            *makeMLP(128, 3, nn.Sigmoid)
+        )
 
     # for coarse network, input is obtained by sampling, sampling result is (ray_num, point_num, 9), (depth) (ray_num, point_num)
-    def forward(self, pts:torch.Tensor, depth:torch.Tensor) -> torch.Tensor:
+    # TODO: fine-network输入的point_num是192，会产生影响吗？
+    def forward(self, pts:torch.Tensor) -> torch.Tensor:
         flat_batch = pts.shape[0] * pts.shape[1]
         position_dim, direction_dim = 6 * self.position_flevel, 6 * self.direction_flevel
         encoded_x:torch.Tensor = torch.zeros(flat_batch, position_dim)
@@ -44,16 +57,19 @@ class NeRF(nn.Module):
         encoded_x = encoded_x.view(pts.shape[0], pts.shape[1], position_dim)
         encoded_x = encoded_x.view(pts.shape[0], pts.shape[1], direction_dim)
 
+        tmp = self.lin_block1(encoded_x)
+        encoded_x = torch.cat((tmp, encoded_x), dim = -1)
+        encoded_x = self.lin_block2(encoded_x)
+        opacity = self.opacity_head(encoded_x)
+        rgb = self.rgb_layer(torch.cat((encoded_x, encoded_r), dim = -1))
+        return torch.cat((rgb, opacity), dim = -1)
 
-        pass
-
-    def render(self, rgbo:torch.Tensor, depth:torch.Tensor) -> torch.Tensor:
-        rgb:torch.Tensor = rgbo[..., :3] # shape (ray_num, pnum, 3)
-        # RGB passed through sigmoid
-        rgb_normed:torch.Tensor = F.sigmoid(rgb)
-
-        opacity:torch.Tensor = rgbo[..., -1]
-        
+    """
+        This function is important for inverse transform sampling, since for every ray
+        we will have 64 normalized weights (summing to 1.) for inverse sampling
+    """
+    @staticmethod
+    def getNormedWeight(opacity:torch.Tensor, depth:torch.Tensor) -> torch.Tensor:
         delta:torch.Tensor = torch.hstack((depth[:, 1:] - depth[:, :-1], torch.FloatTensor([1e9]).repeat((depth.shape[0], 1)).cuda()))
         mult:torch.Tensor = -opacity * delta
 
@@ -61,13 +77,23 @@ class NeRF(nn.Module):
         alpha:torch.Tensor = 1. - torch.exp(mult)       # shape (ray_num, point_num)
         # fusion requires normalization, rgb output should be passed through sigmoid
         weights:torch.Tensor = ts * alpha               # shape (ray_num, point_num)
-        weights_normed:torch.Tensor = torch.sum(weights, dim = -1, keepdim = True)
+        return torch.sum(weights, dim = -1, keepdim = True)
+
+    @staticmethod
+    def render(rgbo:torch.Tensor, depth:torch.Tensor) -> torch.Tensor:
+        rgb:torch.Tensor = rgbo[..., :3] # shape (ray_num, pnum, 3)
+        # RGB passed through sigmoid
+        rgb_normed:torch.Tensor = F.sigmoid(rgb)
+
+        opacity:torch.Tensor = rgbo[..., -1]
+        weights_normed:torch.Tensor = NeRF.getNormedWeight(opacity, depth)
 
         weighted_rgb:torch.Tensor = weights_normed[:, :, None] * rgb_normed
         return torch.sum(weighted_rgb, dim = 1)
 
 """
-TODO:
-- 数据集已经可以加载了，也即coarse网络是完全可以跑起来的，现在还有一个主要问题就是
-    - 只差最后一部分：光线render以及全图render
+Latest TODO:
+- 数据集加载与网络runablity测试
+- test模块（全图sample）
+- 训练可执行文件
 """
---
+++ py/train.py
@@ -0,0 +1,184 @@
+#-*-coding:utf-8-*-
+"""
+    This is a template for torch train.py
+    This is a timm-bounded version
+"""
+import os
+import torch
+import argparse
+from functools import partial
+
+from torch import optim
+
+from torch.cuda import amp
+from timm.utils import NativeScaler
+from timm.models import model_parameters
+
+# from timm.loss import SoftTargetCrossEntropy
+# from timm.scheduler import CosineLRScheduler
+
+default_chkpt_path = "./check_points/"
+default_model_path = "./model/"
+
+torch.backends.cudnn.benchmark = True
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs", type = int, default = 20, help = "Training lasts for . epochs")
+parser.add_argument("--batch_size", type = int, default = 100, help = "Batch size for range image packs.")
+parser.add_argument("--eval_time", type = int, default = 50, help = "Eval every <eval_time> batches.")
+parser.add_argument("--cooldown_epoch", type = int, default = 10, help = "Epochs to cool down lr after cyclic lr.")
+parser.add_argument("--eval_div", type = int, default = 5, help = "Output every <...> times in evaluation")
+parser.add_argument("--name", type = str, default = "model_1.pth", help = "Model name for loading")
+parser.add_argument("--weight_decay", type = float, default = 3e-2, help = "Weight Decay in AdamW")
+parser.add_argument("--max_lr", type = float, default = 55e-5, help = "Max learning rate")
+parser.add_argument("--min_lr", type = float, default = 1e-5, help = "Min learning rate")
+parser.add_argument("-d", "--del_dir", action = "store_true", help = "Delete dir ./logs and start new tensorboard records")
+parser.add_argument("-l", "--load", default = False, action = "store_true", help = "Load checkpoint or trained model.")
+parser.add_argument("-s", "--use_scaler", default = False, action = "store_true", help = "Use AMP scaler to speed up")
+args = parser.parse_args()
+
+# Calculate accurarcy (correct prediction counter)
+def oneHotAccCounter(pred:torch.FloatTensor, truth:torch.FloatTensor)->int:
+    _, pred_max_pos = torch.max(pred, dim = 1)
+    _, gt_max_pos = torch.max(truth, dim = 1)
+    return torch.sum(pred_max_pos == gt_max_pos)
+
+def accCounter(pred:torch.FloatTensor, truth:torch.FloatTensor)->int:
+    # TODO: to be modified
+    return 0
+
+def main():
+    epochs              = args.epochs
+    eval_time           = args.eval_time
+    use_amp             = args.use_scaler
+    batch_size          = args.batch_size
+    del_dir             = args.del_dir
+    use_load            = args.load
+    load_path           = default_model_path + args.name
+
+    # ======= load train set and test set =======
+    train_set = None            # getDataset()
+    test_set = None             # getDataset()
+
+    device = None
+    if not torch.cuda.is_available():
+        print("CUDA not available.")
+        exit(-1)
+    device = torch.device(0)
+    
+    # ======= instantiate model =====
+    # NOTE: model is recommended to have loadFromFile method
+    # model = Model()
+    # if use_load == True and os.path.exists(load_path):
+    #     model.loadFromFile(load_path)
+    # else:
+    #     print("Not loading or load path '%s' does not exist."%(load_path))
+
+    # ======= Loss function ==========
+    # loss_func = SoftTargetCrossEntropy().cuda()
+    # eval_loss_func = torch.nn.CrossEntropyLoss().cuda()
+
+    # ======= Optimizer and scheduler ========
+    # opt = optim.AdamW(model.parameters(), lr = 1.0, betas = (0.9, 0.999), weight_decay=args.weight_decay)
+    # min_max_ratio = args.min_lr / args.max_lr
+    # lec_sch_func = CosineLRScheduler(opt, t_initial = epochs // 2, t_mul = 1, lr_min = min_max_ratio, decay_rate = 0.1,
+    #         warmup_lr_init = min_max_ratio, warmup_t = 10, cycle_limit = 2, t_in_epochs = True)
+    # opt_sch = optim.lr_scheduler.LambdaLR(opt, lr_lambda = partial(get_sch_lr, lec_sch_func, args.max_lr), last_epoch=-1)
+
+    epochs = 0  # lec_sch_func.get_cycle_length() + args.cooldown_epoch
+    
+    # ====== tensorboard summary writer ======
+    # writer = getSummaryWriter(epochs, del_dir)
+
+    amp_scaler = None
+    if use_amp:
+        amp_scaler = NativeScaler()
+    train_cnt = 0
+    for ep in range(epochs):
+        # model.train()
+        train_acc_cnt = 0
+        train_num = 0
+        # writer.add_scalar('Learning Rate', opt_sch.get_last_lr()[-1], ep)
+        
+        for i, (px, py) in enumerate(train_set):
+            px:torch.Tensor = px.cuda()
+            py:torch.Tensor = py.cuda()
+            # ====== Auto Mixture Precision ======
+            # with amp.autocast():
+            #     pred = model(px)
+            #     loss = loss_func(pred, py)
+
+            # ======== Accuracy Counter ========
+            # train_acc_cnt += oneHotAccCounter(pred, py)
+            # train_num += len(pred)
+
+            # ============ optimization and amp =============
+            # opt.zero_grad()
+            # if not amp_scaler is None:
+            #     amp_scaler(loss, opt, clip_grad=None, parameters = model_parameters(model), create_graph = False)
+            # else:
+            #     loss.backward()
+            #     opt.step()
+            # torch.cuda.synchronize()            # Maybe Prefetch Loader needs sync
+
+            if train_cnt % eval_time == 1:
+                train_acc = train_acc_cnt / train_num
+                # ========= Evaluation output ========
+                # print("Traning Epoch: %4d / %4d\t Batch %4d / %4d\t train loss: %.4f\t acc: %.4f\t lr: %f"%(
+                #         ep, epochs, i, batch_num, loss.item(), train_acc, opt_sch.get_last_lr()[-1]
+                # ))
+                # writer.add_scalar('Loss/Train Loss', loss, train_cnt)
+                # writer.add_scalar('Acc/Train Set Accuracy', train_acc, train_cnt)
+                train_num = 0
+                train_acc_cnt = 0
+            train_cnt += 1
+
+        # model.eval()
+        with torch.no_grad():
+            ## +++++++++++ Load from Test set ++++++++=
+            test_acc_cnt = 0
+            test_loss:torch.Tensor = torch.zeros(1, device = device)
+            eval_out_cnt = 0
+            for j, (ptx, pty) in enumerate(test_set):
+                ptx:torch.Tensor = ptx.cuda()
+                pty:torch.Tensor = pty.cuda()
+
+                # =========== Test Evaluation ==========
+                # pred = model(ptx)
+                # test_acc_cnt += accCounter(pred, pty)
+                # test_loss += eval_loss_func(pred, pty)
+                if (j + 1) % 20 == 0:
+                    test_acc = test_acc_cnt / 2000
+                    test_loss /= 2000
+
+                    # ============= Test output ===========
+                    # writer.add_scalar('Loss/Test loss', test_loss, eval_div * ep + eval_out_cnt)
+                    # writer.add_scalar('Acc/Test Set Accuracy', test_acc, eval_div * ep + eval_out_cnt)
+                    # print("Evalutaion in epoch: %4d / %4d\t evalutaion step: %d\t test loss: %.4f\t test acc: %.4f\t lr: %f"%(
+                    #     ep, epochs, eval_out_cnt, test_loss.item(), test_acc, opt_sch.get_last_lr()[-1]
+                    # ))
+                    eval_out_cnt += 1
+                    test_acc_cnt = 0
+                    test_loss.zero_()
+        # ========= A Must: convert model back to training mode ========
+        # model.train()
+
+        # ======== Saving checkpoints ========
+        # torch.save({
+        #     'model': model.state_dict(),
+        #     'optimizer': opt.state_dict()},
+        #     "%schkpt_%d.pt"%(default_chkpt_path, train_cnt)
+        # )
+        # opt_sch.step()
+
+    # ======== Saving the model ========
+    # torch.save({
+    #     'model': model.state_dict(),
+    #     'optimizer': opt.state_dict()},
+    #     "%smodel_4.pth"%(default_model_path)
+    # )
+    # writer.close()
+    print("Output completed.")
+
+if __name__ == "__main__":
+    main()
