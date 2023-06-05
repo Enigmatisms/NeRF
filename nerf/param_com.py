e2f11577fd5d6dfcf98b1b81386a496c5b4be64d
Enigmatisms <984041003@qq.com>
2023-06-05 10:16:02 +0800

    Updated grouping communication method

--- nerf/param_com.py
+++ nerf/param_com.py
@@ -10,16 +10,16 @@ from typing import Union
 from nerf.mip_model import MipNeRF
 from nerf.addtional import ProposalNetwork 
 
-def param_send(model:Union[MipNeRF, ProposalNetwork], dist_ranks: list):
+def param_send(model:Union[MipNeRF, ProposalNetwork], dist_ranks: list, group = None):
     """ Send to specific machine """
     for param in model.parameters():
         for rank in dist_ranks:
-            dist.send(tensor = param.data, dst = rank)
+            dist.send(tensor = param.data, dst = rank, group = group)
             
-def param_recv(model:Union[MipNeRF, ProposalNetwork], source_rank: list):
+def param_recv(model:Union[MipNeRF, ProposalNetwork], source_rank: list, group = None):
     """ Receive to specific machine """
     for param in model.parameters():
-        dist.recv(tensor = param.data, src = source_rank)
+        dist.recv(tensor = param.data, src = source_rank, group = group)
         
 def param_recv_avg(
     model: Union[MipNeRF, ProposalNetwork],
@@ -30,36 +30,26 @@ def param_recv_avg(
     for p_tmp, p_model in zip(tmp.parameters(), model.parameters()):
         p_model.data *= weights[self_rank]
         for src_rank in source_ranks:
-            dist.recv(tensor = p_tmp.data, src = src_rank, group = group)
+            dist.recv(tensor = p_tmp.data, src = src_rank, group = group, group = group)
             p_model.data += weights[src_rank] * p_tmp.data
         
-def param_reduce(model:Union[MipNeRF, ProposalNetwork], weights: list, self_rank: int, dst_rank: int = 0):
+def param_reduce(model:Union[MipNeRF, ProposalNetwork], weights: list, self_rank: int, dst_rank: int = 0, group = None):
     """ Reduce parameter from all other machines 
         Before sending: multiply the param by the weight
     """
     for param in model.parameters():
         param.data *= weights[self_rank]
-        dist.reduce(tensor = param.data, dst = dst_rank)
+        dist.reduce(tensor = param.data, dst = dst_rank, group = group)
         
-def param_broadcast(model:Union[MipNeRF, ProposalNetwork], src_rank: int = 0):
+def param_broadcast(model:Union[MipNeRF, ProposalNetwork], src_rank: int = 0, group = None):
     """ Broadcast parameter to all other machines """
     for param in model.parameters():
-        dist.broadcast(tensor = param.data, src = src_rank)
+        dist.broadcast(tensor = param.data, src = src_rank, group = group)
         
-def param_all_reduce(model:Union[MipNeRF, ProposalNetwork]):
+def param_all_reduce(model:Union[MipNeRF, ProposalNetwork], group = None):
     """ All reduce one step model average (each node is a bottleneck) 
         Before calling this function, all the parameters are weighted
     """
     for param in model.parameters():
-        dist.all_reduce(tensor = param.data)
-    
-def network_average(
-    tmp: Union[MipNeRF, ProposalNetwork],
-    dst: Union[MipNeRF, ProposalNetwork]
-):
-    if tmp.state_dict().keys() != dst.state_dict().keys():
-        raise ValueError("Two networks can not be averaged since they have different state entries.")
-    for p_tmp, p_dst in zip(tmp.parameters(), dst.parameters()):
-        p_dst.data += p_tmp.data
-        p_dst.data *= 0.5
+        dist.all_reduce(tensor = param.data, group = group)
         
